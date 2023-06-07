d0a45199e2b0914a4d62211874217619c1ba0c03
d3nnis2001 <d3nnis.s@web.de>
2023-06-07 02:05:52 +0200

    supertrend runtime reduced by 40x und listen erweiterung mit rrr und beispiel plot

---
+++ src/archieve.py
@@ -0,0 +1,93 @@
+def supertrend(data, lookback=10, multiplier=2.5, band=0):
+    high = data["High"]
+    low = data["Low"]
+    close = data["Close"]
+    # ATR
+    
+    tr1 = pd.DataFrame(high - low)
+    tr2 = pd.DataFrame(abs(high - close.shift(1)))
+    tr3 = pd.DataFrame(abs(low - close.shift(1)))
+    frames = [tr1, tr2, tr3]
+    tr = pd.concat(frames, axis = 1, join = 'inner').max(axis = 1)
+    atr = tr.ewm(lookback).mean()
+    
+    # H/L AVG AND BASIC UPPER & LOWER BAND
+    
+    hl_avg = (high + low) / 2
+    upper_band = (hl_avg + multiplier * atr).fillna(method="ffill").dropna()
+    lower_band = (hl_avg - multiplier * atr).dropna()
+    
+    # FINAL UPPER BAND
+    final_bands = pd.DataFrame(columns = ['upper', 'lower'])
+    final_bands.iloc[:,0] = [x for x in upper_band - upper_band]
+    final_bands.iloc[:,1] = final_bands.iloc[:,0]
+    for i in range(len(final_bands)):
+        if i == 0:
+            final_bands.iloc[i,0] = 0
+        else:
+            if (upper_band[i] < final_bands.iloc[i-1,0]) | (close[i-1] > final_bands.iloc[i-1,0]):
+                final_bands.iloc[i,0] = upper_band[i]
+            else:
+                final_bands.iloc[i,0] = final_bands.iloc[i-1,0]
+    
+    # FINAL LOWER BAND
+    
+    for i in range(len(final_bands)):
+        if i == 0:
+            final_bands.iloc[i, 1] = 0
+        else:
+            if (lower_band[i] > final_bands.iloc[i-1,1]) | (close[i-1] < final_bands.iloc[i-1,1]):
+                final_bands.iloc[i,1] = lower_band[i]
+            else:
+                final_bands.iloc[i,1] = final_bands.iloc[i-1,1]
+    
+    # SUPERTREND
+    
+    supertrend = pd.DataFrame(index=upper_band.index, columns=[f'supertrend_{lookback}'])
+    supertrend.iloc[:, 0] = upper_band.values
+    supertrend.iloc[:,0] = [x for x in final_bands['upper'] - final_bands['upper']]
+    
+    for i in range(1, len(supertrend)):
+        if i == 0:
+            supertrend.iloc[i, 0] = 0
+        elif supertrend.iloc[i-1, 0] == final_bands.iloc[i-1, 0] and close[i] < final_bands.iloc[i, 0]:
+            supertrend.iloc[i, 0] = final_bands.iloc[i, 0]
+        elif supertrend.iloc[i-1, 0] == final_bands.iloc[i-1, 0] and close[i] > final_bands.iloc[i, 0]:
+            supertrend.iloc[i, 0] = final_bands.iloc[i, 1]
+        elif supertrend.iloc[i-1, 0] == final_bands.iloc[i-1, 1] and close[i] > final_bands.iloc[i, 1]:
+            supertrend.iloc[i, 0] = final_bands.iloc[i, 1]
+        elif supertrend.iloc[i-1, 0] == final_bands.iloc[i-1, 1] and close[i] < final_bands.iloc[i, 1]:
+            supertrend.iloc[i, 0] = final_bands.iloc[i, 0]
+    
+    supertrend = supertrend.set_index(upper_band.index)
+    supertrend = supertrend.dropna()[1:]
+    
+    # ST UPTREND/DOWNTREND
+    
+    upt = []
+    dt = []
+    close = close.iloc[len(close) - len(supertrend):]
+
+    for i in range(len(supertrend)):
+        if close[i] > supertrend.iloc[i, 0]:
+            upt.append(supertrend.iloc[i, 0])
+            dt.append(np.nan)
+        elif close[i] < supertrend.iloc[i, 0]:
+            upt.append(np.nan)
+            dt.append(supertrend.iloc[i, 0])
+        else:
+            upt.append(np.nan)
+            dt.append(np.nan)
+    new_index = pd.date_range(start= data.index[0] , periods=1, freq='3min')
+    st, upt, dt = pd.Series(supertrend.iloc[:, 0]), pd.Series(upt), pd.Series(dt)
+    upt.index, dt.index = supertrend.index, supertrend.index
+    upt = pd.concat([pd.Series([np.nan], index=new_index), upt])
+    dt = pd.concat([pd.Series([np.nan], index=new_index), dt])
+    data["upt"] = upt
+    data["upt"].index = data.index
+    data["dt"] = dt
+    data["dt"].index = data.index
+    if(band==0):
+        return data["dt"]
+    elif(band==1):
+        return data["upt"]
